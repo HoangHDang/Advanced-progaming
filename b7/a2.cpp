40fb95816521e1b6ed8467395ff9becc5c5bf3e7
vdvkl <44463603+vdvkl@users.noreply.github.com>
2019-03-22 14:59:45 +0700

    ss

--- b7/a2.cpp
+++ b7/a2.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 void f1(int a[])
 {
-    cout<<sizeof(a);
+    cout<<sizeof(a)<<" ";
 
 }
 void f2(int a[10])
 {
-    cout<<sizeof(a);
+    cout<<sizeof(a)<<" ";
 }
 int main()
 {
     int a[100];
-cout<<sizeof(a);
+cout<<sizeof(a)<<" ";
 f1(a);
 f2(a);
 
